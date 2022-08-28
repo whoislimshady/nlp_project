bcb751643c9dad04b3e9c527d64225867dcc9f5e
harsh <2028202@kiit.ac.in>
2022-08-28 22:55:34 +0530

    api keys updated

--- main.py
+++ main.py
@@ -12,15 +12,40 @@ import matplotlib.pyplot as plt
 from collections import Counter
 app = Flask(__name__)
     
-consumer_key =''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
+consumer_key = "DCvosorD3gRN6CBzG0EEFJPDD"
+consumer_secret = "mjF1I5PtmDKu9FblHujIzDTwhFWapR8kIXCSUXXmbNCV8BOhLx" 
+access_key = "1072199779181047809-00lZwdHtduYHbunpaWtSWACAsksZuu"
+access_secret = "YUTCqBViyU4ZejJOQEDD92A11nEdMxbyvtMPTzlnFIytk" 
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, wait_on_rate_limit=True,
-                 wait_on_rate_limit_notify=True)
+def percentage(part,whole):
+     return 100 * float(part)/float(whole)
+
+def twitter_setup():
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+    
+    try:
+        api.verify_credentials()
+        print("Successful Authentication")
+    except:
+        print("Failed authentication")
+    return api
+
+
+extractor = twitter_setup()
+
+
+def keywords_tweets(api, keyword, number_of_tweets):
+    new_keyword = keyword + " -filter:retweets"
+    tweets = []
+    for status in tweepy.Cursor(api.search_tweets, q=new_keyword, 
+                                lang="en", tweet_mode='extended', 
+                                result_type='mixed').items(number_of_tweets):
+          tweets.append(status)
+    return tweets
 
 if (not api):
     print("Problem Connecting to API")
@@ -34,97 +59,62 @@ def query_model():
   number = req_data['number']
   location = req_data['location']
 
-  places = api.geo_search(query=location)
+  # Grab Currrent Time Before Running the Code
 
-  print(places[0])
-
-  tweetsPerQry = 100  
-
-  sinceId = None
-
-
-  max_id = -1
-  data = None
-
-  dataset = []
-  outputFile = 'test.data'
-  fw = open(outputFile, 'wb')
-
-  tweetCount = 0
-  print("Downloading max {0} tweets".format(number))
-  while tweetCount < number:
-      try:
-          if (max_id <= 0):
-              if (not sinceId):
-                  new_tweets = api.search(q=hash_tag, count=tweetsPerQry, tweet_mode='extended', lang='en')
-                  tweetCount += len(new_tweets)
-                  print("Downloaded {0} tweets".format(tweetCount))
-              else:
-                  new_tweets = api.search(q=hash_tag, count=tweetsPerQry,
-                                          since_id=sinceId, tweet_mode='extended', lang='en')
-                  print("here 2")
-          else:
-              if (not sinceId):
-                  new_tweets = api.search(q=hash_tag, count=tweetsPerQry,
-                                          max_id=str(max_id - 1), tweet_mode='extended', lang='en')
-                  print("here 3")
-              else:
-                  new_tweets = api.search(q=hash_tag, count=tweetsPerQry,
-                                          max_id=str(max_id - 1),
-                                          since_id=sinceId, tweet_mode='extended', lang='en')
-                  print("here 4")
-              if not new_tweets:
-                  print("No more tweets found")
-                  break
-
-          # print(new_tweets)
-
-          tweets = new_tweets
-          for tweet in tweets:
-              dataset.append(tweet.full_text)
-          #data += pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
-          tweetCount += len(new_tweets)
-          print("Downloaded {0} tweets".format(tweetCount))
-          max_id = new_tweets[-1].id
-      except tweepy.TweepError as e:
-          print(str(e))
-          break
-
-
-  pickle.dump(dataset, fw)
-  fw.close()
-  import nltk
-  inputFile = 'test.data'
-  fd = open(inputFile, 'rb')
-  dataset = pickle.load(fd)
-  print(dataset)
+   # Scrape tweets based on keywords
+  keyword_alltweets = keywords_tweets(extractor,hash_tag, number)
 
 
 
-  nltk.download('vader_lexicon')
+  data = pd.DataFrame(data=[tweet.full_text for tweet in keyword_alltweets],columns=['Tweets'])
+  data['Tweets_ID'] = [tweet.id for tweet in keyword_alltweets]
+  data['Date'] = [tweet.created_at for tweet in keyword_alltweets]
+  data['Source'] = [tweet.source for tweet in keyword_alltweets]
+  data['Likes_no'] = [tweet.favorite_count for tweet in keyword_alltweets]
+  data['Retweets_no'] = [tweet.retweet_count for tweet in keyword_alltweets]
+  data['Location'] = [tweet.user.location for tweet in keyword_alltweets]
+  data['UID'] = [tweet.user.id for tweet in keyword_alltweets]
+  data['Username'] = [tweet.user.screen_name for tweet in keyword_alltweets]
+  data['DisplayName'] = [tweet.user.name for tweet in keyword_alltweets]
+  data['Verified'] = [tweet.user.verified for tweet in keyword_alltweets]  
+  data['ProfileLink'] = [tweet.user.screen_name for tweet in keyword_alltweets]
+  data['Tweet_URL'] = [tweet.id_str for tweet in keyword_alltweets]
+  for i in range(len(keyword_alltweets)):
+       data['Tweet_URL'][i]="https://twitter.com/"+data['ProfileLink'][i] +"/status/"+data['Tweet_URL'][i]
+       data['ProfileLink'][i] = "https://twitter.com/" + data['ProfileLink'][i]  
+  data.to_csv('file1.csv')
+  positive = 0
+  negative = 0
+  neutral = 0
+  polarity = 0
+  tweet_list = []
+  neutral_list = []
+  negative_list = []
+  positive_list = []
+  for tweet in data['tweets']:
+   
+   #print(tweet.text)
+      analysis = TextBlob(tweet)
+      score = SentimentIntensityAnalyzer().polarity_scores(tweet)
+      neg = score['neg']
+      neu = score['neu']
+      pos = score['pos']
+      comp = score['compound']
+      polarity += analysis.sentiment.polarity
+      
+      if neg > pos:
+          negative_list.append(tweet)
+          negative += 1
+      elif pos > neg:
+          positive_list.append(tweet)
+          positive += 1
+          
+      elif pos == neg:
+          neutral_list.append(tweet)
+          neutral += 1
+      return positive,negative,neutral,polarity
 
-  sid = SentimentIntensityAnalyzer()
 
-
-  l = []
-  counter = Counter()
-
-  for data in dataset:
-      ss = sid.polarity_scores(data)
-      l.append(ss)
-      k = ss['compound']
-      if k >= 0.05:
-          counter['positive'] += 1
-      elif k <= -0.05:
-          counter['negative'] += 1
-      else:
-          counter['neutral'] += 1
-
-  positive = counter['positive']
-  negative = counter['negative']
-  neutral = counter['neutral']
-
-  return positive,negative,neutral
 if __name__ == "__main__":
 
     app.run(host='0.0.0.0', port=5000)
